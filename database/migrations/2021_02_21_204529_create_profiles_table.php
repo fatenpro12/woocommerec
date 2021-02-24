c3c90f31d39571f6023d29a32d0dbcf02faff34e
Osama203-code <osamasoliman284@gmail.com>
2021-02-25 01:59:00 +0400

    profile

---
+++ database/migrations/2021_02_21_204529_create_profiles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('supplier_name');
+            $table->string('supplier_email');
+            $table->string('supplier_place');
+            $table->string('supplier_mobile');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
